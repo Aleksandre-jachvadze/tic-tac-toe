d7e0a7b06975461e57b22c020ad85c8e05de99a5
aleksandre jachvadze <155845898+Aleksandre-jachvadze@users.noreply.github.com>
2024-08-26 17:02:29 +0400

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+
+function MyComponent() {
+  return <div>Hello, React Testing Library!</div>;
+}
+
+export default MyComponent;
